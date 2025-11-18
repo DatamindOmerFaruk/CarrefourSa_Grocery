08b43460d3f777c56965a90fc8145e5c33f3d6b4
unknown <omer.keklik@datamind.com>
2025-11-18 10:03:43 +0300

    s3 upload güncellenmesi v8

--- test.py
+++ test.py
@@ -1,26 +1,25 @@
 import boto3
+import os
 
-ACCESS_KEY = "sWxdTl3ERx7myBE1qpW06_haVvuhATcdsmBbqaWkXYU"
-SECRET_KEY = "Ti9Fonk3wYyG5PMx5LaGUmlcVyCuqsE5BLVV5vv8PU0"
-ENDPOINT_URL = "https://361cohesity.carrefoursa.com:3000"
-BUCKET_NAME = "Grocery"
+# S3 Ayarları (environment variables'dan alınır)
+S3_ENDPOINT_URL = os.getenv("S3_ENDPOINT_URL", "https://161cohesity.carrefoursa.com:3000")
+S3_ACCESS_KEY_ID = os.getenv("S3_ACCESS_KEY_ID", "")
+S3_SECRET_ACCESS_KEY = os.getenv("S3_SECRET_ACCESS_KEY", "")
+S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME", "Grocery")
 
 def upload_image(local_image_path, object_name):
     s3 = boto3.client(
         "s3",
-        endpoint_url=ENDPOINT_URL,
-
-        aws_access_key_id=ACCESS_KEY,
-
-        aws_secret_access_key=SECRET_KEY,
-
+        endpoint_url=S3_ENDPOINT_URL,
+        aws_access_key_id=S3_ACCESS_KEY_ID,
+        aws_secret_access_key=S3_SECRET_ACCESS_KEY,
         verify=False  # Gerekirse SSL hatalarında kapatılabilir
 
     )
 
-    s3.upload_file(local_image_path, BUCKET_NAME, object_name)
+    s3.upload_file(local_image_path, S3_BUCKET_NAME, object_name)
 
-    print(f"{local_image_path} → s3://{BUCKET_NAME}/{object_name} yüklendi.")
+    print(f"{local_image_path} → s3://{S3_BUCKET_NAME}/{object_name} yüklendi.")
 
 if __name__ == "__main__":
 
